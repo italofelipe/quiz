e4f86b8a2f295d1635da2d1d26278d3f2891f3c3
italofelipe <felipe.italo@hotmail.com>
2022-05-26 15:36:02 -0300

    chore: generated setup files

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Basement App</title>
+        <meta
+          name="description"
+          content="A quiz application made with Next.js"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Basement app</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
