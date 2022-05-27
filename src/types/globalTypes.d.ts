46e34ef616ffc55d6d82f90bfc90b62a79181f5e
italofelipe <felipe.italo@hotmail.com>
2022-05-27 01:51:52 -0300

    feat: created global types to define components data and api responses types

---
+++ src/types/globalTypes.d.ts
@@ -0,0 +1,63 @@
+type CreateRoundData = {
+  round: {
+    player_name: string;
+    category_id: number;
+  };
+};
+
+type Round = {
+  id: number;
+  player_id: number;
+  questions: Question[];
+  answers: Answer[];
+};
+
+type Question = {
+  id: number;
+  description: string;
+  options: QuestionOption[];
+};
+
+type QuestionOption = {
+  id: number;
+  label: string;
+};
+
+type Answer = {
+  question_id: string;
+  option_id: string;
+};
+
+type Categories = {
+  categories: {
+    id: number;
+    name: string;
+  };
+};
+
+type AxiosHandler = {
+  config: {
+    path: "/rounds" | "/categories";
+    params?: string;
+  };
+  body: SendRoundResponseRequest | SendRoundRequest;
+};
+
+type SendRoundResponseRequest = {
+  answer: {
+    question_id: number;
+    option_id: number;
+  };
+};
+
+type SendRoundRequest = {
+  round: {
+    player_name: string;
+    category_id: number;
+  };
+};
+
+type InitialStepData = {
+  category: number;
+  player_name: string;
+};
